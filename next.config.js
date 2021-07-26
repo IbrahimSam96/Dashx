9e55933f458fe601fb2152251aab66f26679db13
Ibrahimsam96 <Ibrahimsam96@Gmail.com>
2021-07-26 14:01:57 -0400

    Configjs EnviromentVariables

--- next.config.js
+++ next.config.js
@@ -3,3 +3,12 @@ const withVideos = require('next-videos')
  
 module.exports = withVideos()
 
+module.exports = {
+    env: {
+        PRIVATE_KEY: process.env.PRIVATE_KEY,
+        CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+        PROJECT_ID: process.env.PROJECT_ID,
+        NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID
+    }
+  };
+  
